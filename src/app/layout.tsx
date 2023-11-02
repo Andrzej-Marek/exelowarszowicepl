b70358050969054f9afd2a07c87760fd67012d16
Andrzej Marek <andrzejmarek@Andrzejs-Mac-Studio.local>
2023-11-02 11:18:58 +0100

    Pixel route change

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,10 +8,10 @@ import "@/css/style.css";
 
 import { Inter } from "next/font/google";
 import "./globals.css";
-// import Script from "next/script";
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import Script from "next/script";
+import FacebookPixel from "@/lib/FacebookPixel";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -37,29 +37,7 @@ export default function RootLayout({
          gtag('config', 'G-CDTGQFP7XH')
         `}
       </Script>
-      <Script id="meta-pixel">
-        {`
-        !function(f,b,e,v,n,t,s)
-        {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-        n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-        if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-        n.queue=[];t=b.createElement(e);t.async=!0;
-        t.src=v;s=b.getElementsByTagName(e)[0];
-        s.parentNode.insertBefore(t,s)}(window, document,'script',
-        'https://connect.facebook.net/en_US/fbevents.js');
-        fbq('init', '185043937985431');
-        fbq('track', 'PageView');
-        `}
-      </Script>
-
-      <noscript>
-        <img
-          height="1"
-          width="1"
-          style={{ display: "none" }}
-          src="https://www.facebook.com/tr?id=185043937985431&ev=PageView&noscript=1"
-        />
-      </noscript>
+      <FacebookPixel />
 
       {/* <head>
         <link rel="icon" href="/favicon.svg" sizes="any" />
