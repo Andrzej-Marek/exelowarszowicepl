a626a701a0ea52f894606487f13e3ab9e5259d6c
Andrzej Marek <andrzejmarek@Andrzejs-Mac-Studio.local>
2023-11-02 11:14:18 +0100

    GA and meta pixel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,6 +11,7 @@ import "./globals.css";
 // import Script from "next/script";
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -26,6 +27,40 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" data-bs-theme="dark">
+      <Script src="https://www.googletagmanager.com/gtag/js?id=G-CDTGQFP7XH" />
+      <Script id="google-analytics">
+        {`
+         window.dataLayer = window.dataLayer || [];
+         function gtag(){dataLayer.push(arguments);}
+         gtag('js', new Date());
+       
+         gtag('config', 'G-CDTGQFP7XH')
+        `}
+      </Script>
+      <Script id="meta-pixel">
+        {`
+        !function(f,b,e,v,n,t,s)
+        {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+        n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+        if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+        n.queue=[];t=b.createElement(e);t.async=!0;
+        t.src=v;s=b.getElementsByTagName(e)[0];
+        s.parentNode.insertBefore(t,s)}(window, document,'script',
+        'https://connect.facebook.net/en_US/fbevents.js');
+        fbq('init', '185043937985431');
+        fbq('track', 'PageView');
+        `}
+      </Script>
+
+      <noscript>
+        <img
+          height="1"
+          width="1"
+          style={{ display: "none" }}
+          src="https://www.facebook.com/tr?id=185043937985431&ev=PageView&noscript=1"
+        />
+      </noscript>
+
       {/* <head>
         <link rel="icon" href="/favicon.svg" sizes="any" />
       </head> */}
