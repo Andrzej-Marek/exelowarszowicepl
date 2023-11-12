e0cd67794730e3927a5bae9bb28c43867f66d2ba
Andrzej Marek <andrzejmarek@Andrzejs-Mac-Studio.local>
2023-11-12 07:42:55 +0100

    Host

--- src/utils/buildCMSUrl.tsx
+++ src/utils/buildCMSUrl.tsx
@@ -1,5 +1,5 @@
-const host = process.env.NEXT_PUBLIC_CMS_BASE_URL;
-
 export const buildCMSUrl = (path: string) => {
+  const host = process.env.NEXT_PUBLIC_CMS_BASE_URL;
+  console.log("host", host);
   return `${host}/${path}`.replaceAll("//", "/");
 };
